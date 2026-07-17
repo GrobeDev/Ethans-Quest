046370bf90d650708e0b761ff833607767917caa
Robin Moss <robinmoss@Robins-MacBook-Pro.local>
2026-07-17 17:10:01 +0100

    Add initial Xcode project for iOS app

---
+++ ios/EthansQuest/EthansQuest/EthansQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthansQuestApp.swift
+//  EthansQuest
+//
+//  Created by Robin Moss on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct EthansQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
